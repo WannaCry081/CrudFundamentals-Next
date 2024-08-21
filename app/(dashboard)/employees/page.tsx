fb6243c3c46b9a464f0d2ea39d060ec11bd77901
Lirae Que Data <liraedata59@gmail.com>
2024-08-22 01:26:12 +0800

    Create employees/page.tsx

---
+++ app/(dashboard)/employees/page.tsx
@@ -0,0 +1,13 @@
+export default function Employee() {
+  return (
+    <article>
+      <section>
+        <h1>Employee</h1>
+      </section>
+
+      <section>
+
+      </section>
+    </article>
+  );
+}
