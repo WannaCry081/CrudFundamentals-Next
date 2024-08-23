5b584df47201b585af388d51b14cf14960072f34
Lirae Que Data <liraedata59@gmail.com>
2024-08-23 17:51:52 +0800

    Create update employee query

---
+++ hooks/employees/useUpdateEmployeeQuery.ts
@@ -0,0 +1,33 @@
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { QueryClient, useMutation } from "@tanstack/react-query";
+import { UpdateEmployee } from "@/actions";
+import { Employee } from "@/schemas";
+import toast from "react-hot-toast";
+
+const useUpdateEmployeeQuery = (id: string) => {
+  const router = useRouter();
+  const queryClient = new QueryClient();
+  const [isPending, startTransition] = useTransition();
+
+  const { mutate: updateEmployee } = useMutation({
+    mutationFn: (values: Employee) => UpdateEmployee(id, values),
+    onSuccess: () => {
+      startTransition(() => {
+        queryClient.invalidateQueries({
+          queryKey: ["employee", id],
+          exact: true,
+        });
+      });
+      router.push("/employees");
+      toast.success("Successfully Updated Employee");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { isPending, isUpdatePending: isPending, updateEmployee };
+};
+
+export default useUpdateEmployeeQuery;
