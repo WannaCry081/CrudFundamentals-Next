2b355d765f453d4874b3ea6354e0b7272d53cf2f
Lirae Que Data <liraedata59@gmail.com>
2024-08-23 17:52:24 +0800

    Update hooks export

---
+++ hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useEmployeeQuery } from "@/hooks/employees/useEmployeeQuery";
+export { default as useEmployeesQuery } from "@/hooks/employees/useEmployeesQuery";
+export { default as useCreateEmployeeQuery } from "@/hooks/employees/useCreateEmployeeQuery";
+export { default as useUpdateEmployeeQuery } from "@/hooks/employees/useUpdateEmployeeQuery";
+export { default as useDeleteEmployeeQuery } from "@/hooks/employees/useDestroyEmployeeQuery";
