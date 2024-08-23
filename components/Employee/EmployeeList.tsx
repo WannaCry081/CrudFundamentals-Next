f64b3ec5f16bf4b20d25ae39db56aa7545c35ebf
Lirae Que Data <liraedata59@gmail.com>
2024-08-23 17:53:08 +0800

    Fix no employee found condition

--- components/Employee/EmployeeList.tsx
+++ components/Employee/EmployeeList.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEmployeesQuery } from "@/hooks";
 import Link from "next/link";
+import { Edit2Icon } from "lucide-react";
+import { useEmployeesQuery } from "@/hooks";
 
 const EmployeeList = () => {
   const { data, error, isLoading } = useEmployeesQuery();
@@ -13,17 +14,20 @@ const EmployeeList = () => {
     return <div>Error...</div>;
   }
 
-  if (!data && data!.length === 0) {
-    return <div>No resource found</div>;
+  if (!data || data!.length === 0) {
+    return <div>No employee found...</div>;
   }
 
   return (
     <div>
       <ul>
         {data?.map(({ id, firstName, lastName }) => (
-          <li key={id}>
+          <li key={id} className="flex items-center">
             <Link href={`employees/${id}`}>
               {firstName} {lastName}
+            </Link>
+            <Link href={`employees/${id}/edit`}>
+              <Edit2Icon />
             </Link>
           </li>
         ))}
