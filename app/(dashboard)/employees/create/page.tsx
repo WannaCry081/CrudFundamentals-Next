c07791caa0f5e86f87099cdd3d961998f08c34e2
Lirae Que Data <liraedata59@gmail.com>
2024-08-23 17:55:35 +0800

    Update create employee page

---
+++ app/(dashboard)/employees/create/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import EmployeeForm from "@/components/Employee/EmployeeForm";
+import { useCreateEmployeeQuery } from "@/hooks";
+import { Employee } from "@/schemas";
+
+export default function Page() {
+  const { isPending, createEmployee } = useCreateEmployeeQuery();
+  const onSubmit = (values: Employee) => createEmployee(values);
+
+  return (
+    <section>
+      <h1>Create Employees</h1>
+
+      <EmployeeForm isPending={isPending} onSubmit={onSubmit} />
+    </section>
+  );
+}
