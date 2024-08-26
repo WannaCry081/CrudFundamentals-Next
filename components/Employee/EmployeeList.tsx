b8185c4b42283f1cac7a65bb1239c22ef2481751
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 02:59:32 +0800

    Update employee list  component style

--- components/Employee/EmployeeList.tsx
+++ components/Employee/EmployeeList.tsx
@@ -1,38 +1,52 @@
 "use client";
 import Link from "next/link";
-import { Edit2Icon } from "lucide-react";
 import { useEmployeesQuery } from "@/hooks";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import EmployeeListState from "./EmployeeListState";
 
 const EmployeeList = () => {
-  const { data, error, isLoading } = useEmployeesQuery();
+  const { data, isLoading, isError } = useEmployeesQuery();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <EmployeeListState.LoadingState />;
   }
 
-  if (error) {
-    return <div>Error...</div>;
+  if (isError) {
+    return <EmployeeListState.ErrorState />;
   }
 
   if (!data || data!.length === 0) {
-    return <div>No employee found...</div>;
+    return <EmployeeListState.NoContentState />;
   }
 
   return (
-    <div>
-      <ul>
-        {data?.map(({ id, firstName, lastName }) => (
-          <li key={id} className="flex items-center">
-            <Link href={`employees/${id}`}>
-              {firstName} {lastName}
-            </Link>
-            <Link href={`employees/${id}/edit`}>
-              <Edit2Icon />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="space-y-2">
+      {data?.map(({ id, userName, email }) => (
+        <li
+          key={id}
+          className="group rounded-lg p-4 bg-white transition-all ease-in hover:bg-indigo-500"
+        >
+          <Link href={`employees/${id}`} className="w-full">
+            <div className="flex items-center space-x-4">
+              <Avatar className="inline-flex items-center justify-center rounded-full bg-indigo-500 size-10 group-hover:bg-background">
+                <AvatarImage />
+                <AvatarFallback className="group-hover:text-indigo-500 text-white">
+                  AC
+                </AvatarFallback>
+              </Avatar>
+              <span className="flex-1">
+                <h5 className="text-sm font-semibold group-hover:text-background">
+                  {userName}
+                </h5>
+                <p className="text-xs text-neutral-400 group-hover:text-neutral-200">
+                  {email}
+                </p>
+              </span>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
