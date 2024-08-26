88216f7b1d500594ec69a555aa4ab78afa2637d0
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 03:05:30 +0800

    Update styling in dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -6,8 +6,8 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <main>
-      {children}
+    <main className="min-h-svh bg-neutral-100">
+      <div className="p-4 sm:p-6 md:p-8">{children}</div>
     </main>
   );
 }
