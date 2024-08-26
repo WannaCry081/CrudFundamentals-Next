ab3710c2caffcde10076d5620bc5a9fcb5bbf75a
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 03:06:52 +0800

    Update create employee form style

--- app/(dashboard)/employees/create/page.tsx
+++ app/(dashboard)/employees/create/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { EmployeeForm } from "@/components/employee";
+import { BackButton } from "@/components/shared";
 import { useCreateEmployeeQuery } from "@/hooks";
-import { Employee } from "@/schemas";
 
 export default function Page() {
   const { isPending, createEmployee } = useCreateEmployeeQuery();
-  const onSubmit = (values: Employee) => createEmployee(values);
 
   return (
-    <section>
-      <h1>Create Employees</h1>
+    <article>
+      <section className="my-4">
+        <BackButton />
+      </section>
 
-      <EmployeeForm isPending={isPending} onSubmit={onSubmit} />
-    </section>
+      <section className="mb-4">
+        <h1 className="text-2xl font-bold text-indigo-500">Add new Employee</h1>
+      </section>
+
+      <EmployeeForm isPending={isPending} onSubmit={createEmployee} />
+    </article>
   );
 }
