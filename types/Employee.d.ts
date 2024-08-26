8be2f2790d2b0ae389778b49f96514c1e2996fac
Lirae Que Data <liraedata59@gmail.com>
2024-08-26 23:44:44 +0800

    Add update employee types

--- types/Employee.d.ts
+++ types/Employee.d.ts
@@ -1,5 +1,18 @@
+import { PositionType } from "@/types/EmployeeEnum";
+
 export type Employee = {
   id: number;
+  userName: string;
   firstName: string;
   lastName: string;
+  email: string;
+  position: PositionType;
+  phoneNumber: string;
+};
+
+export type UpdateEmployee = {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
 };
