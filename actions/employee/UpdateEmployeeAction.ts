7cfcc4324d7cae913ceea3813c8cd084467e1c47
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 02:54:05 +0800

    Rename actions directory

---
+++ actions/employee/UpdateEmployeeAction.ts
@@ -0,0 +1,26 @@
+"use server";
+import { EmployeeSchema, type EmployeeSchemaType } from "@/schemas";
+import { EmployeeService } from "@/services";
+
+const UpdateEmployeeAction = async (id: string, values: EmployeeSchemaType) => {
+  const { data, success, error } = EmployeeSchema.safeParse(values);
+
+  if (!success) {
+    throw new Error(
+      "Invalid data: " + error.errors.map((error) => error.message).join(",")
+    );
+  }
+
+  try {
+    const response = await EmployeeService.update(id, data);
+    if (response.status !== 200) {
+      throw new Error("Error occurred in updating data");
+    }
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+};
+
+export default UpdateEmployeeAction;
--- actions/index.ts
+++ actions/index.ts
@@ -1,3 +1,3 @@
-export { default as CreateEmployeeAction } from "@/actions/employees/CreateEmployeeAction";
-export { default as UpdateEmployeeAction } from "@/actions/employees/UpdateEmployeeAction";
-export { default as DestroyEmployeeAction } from "@/actions/employees/DestroyEmployeeAction";
+export { default as CreateEmployeeAction } from "@/actions/employee/CreateEmployeeAction";
+export { default as UpdateEmployeeAction } from "@/actions/employee/UpdateEmployeeAction";
+export { default as DestroyEmployeeAction } from "@/actions/employee/DestroyEmployeeAction";
