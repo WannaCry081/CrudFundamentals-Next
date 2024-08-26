0ac8a0b79b1b10895392e41ca5efc22e8854357a
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 02:59:11 +0800

    Update employee form component style

--- components/Employee/EmployeeForm.tsx
+++ components/Employee/EmployeeForm.tsx
@@ -1,5 +1,16 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { When } from "react-if";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Form,
   FormControl,
@@ -8,16 +19,14 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { EmployeeSchema, type Employee } from "@/schemas";
-import { useForm } from "react-hook-form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import { When } from "react-if";
+import { EmployeeSchema, type EmployeeSchemaType } from "@/schemas";
+import { PositionType } from "@/types";
+import { twJoin } from "tailwind-merge";
 
 interface EmployeeFormProps {
   isPending: boolean;
-  initialData?: Employee;
-  onSubmit: (values: Employee) => void;
+  initialData?: EmployeeSchemaType;
+  onSubmit: (values: EmployeeSchemaType) => void;
   onDelete?: () => void;
 }
 
@@ -27,26 +36,96 @@ const EmployeeForm = ({
   onSubmit,
   onDelete,
 }: EmployeeFormProps) => {
-  const form = useForm<Employee>({
+  const form = useForm<EmployeeSchemaType>({
     resolver: zodResolver(EmployeeSchema),
     defaultValues: initialData! || {
+      userName: "",
       firstName: "",
       lastName: "",
+      email: "",
+      position: "",
+      phoneNumber: "",
     },
   });
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="relative space-y-2"
+      >
         <FormField
           control={form.control}
-          name="firstName"
+          name="userName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>First Name</FormLabel>
+              <FormLabel>Username</FormLabel>
               <FormControl>
                 <Input
-                  placeholder="First Name"
+                  className="bg-background"
+                  placeholder="Username"
+                  {...field}
+                  disabled={isPending}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex space-x-4 items-center">
+          <span className="min-w-1/2 w-full max-w-1/2">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-background"
+                      placeholder="First Name"
+                      {...field}
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </span>
+
+          <span className="min-w-1/2 w-full max-w-1/2">
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-background"
+                      placeholder="Last Name"
+                      {...field}
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </span>
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl>
+                <Input
+                  className="bg-background"
+                  placeholder="Email Address"
                   {...field}
                   disabled={isPending}
                 />
@@ -56,34 +135,81 @@ const EmployeeForm = ({
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="lastName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Last Name</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Last Name"
-                  {...field}
-                  disabled={isPending}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="flex space-x-4 items-center">
+          <span className="min-w-1/2 w-full max-w-1/2">
+            <FormField
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone Number</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-background"
+                      placeholder="+63976 123 1234"
+                      {...field}
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </span>
+          <span className="min-w-1/2 w-full max-w-1/2">
+            <FormField
+              control={form.control}
+              name="position"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Position</FormLabel>
+                  <Select
+                    disabled={isPending}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="bg-background">
+                      <SelectValue {...field} placeholder="Select Position" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value={PositionType.MANAGER.toString()}>
+                        Project Manager
+                      </SelectItem>
+                      <SelectItem value={PositionType.DEVELOPER.toString()}>
+                        Developer
+                      </SelectItem>
+                      <SelectItem value={PositionType.TESTER.toString()}>
+                        QA Tester
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </span>
+        </div>
 
-        <When condition={!!initialData}>
-          <Button variant="destructive" onClick={onDelete}>
-            Delete
+        <div className={twJoin("pt-4", !!initialData && "flex gap-2")}>
+          <Button
+            type="submit"
+            disabled={isPending}
+            className="w-full rounded-md  bg-indigo-500 hover:bg-indigo-500/80"
+          >
+            {initialData ? "Update" : "Submit"}
           </Button>
-        </When>
-
-        <Button type="submit" disabled={isPending}>
-          {initialData ? "Update" : "Submit"}
-        </Button>
+        </div>
       </form>
+
+      <When condition={!!initialData}>
+        <Button
+          variant="destructive"
+          onClick={onDelete}
+          className="mt-2 w-full"
+        >
+          Delete
+        </Button>
+      </When>
     </Form>
   );
 };
