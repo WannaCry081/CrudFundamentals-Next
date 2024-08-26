4b2fb0255636abb2a44190a79de8ee22d2d2fea7
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 02:59:52 +0800

    Create employee form state component

---
+++ components/Employee/EmployeeFormState.tsx
@@ -0,0 +1,13 @@
+const EmployeeFormState = {
+  LoadingState: () => {
+    return <div>Loading</div>;
+  },
+  ErrorState: () => {
+    return <div>Loading</div>;
+  },
+  NotFoundState: () => {
+    return <div>Loading</div>;
+  },
+};
+
+export default EmployeeFormState;
