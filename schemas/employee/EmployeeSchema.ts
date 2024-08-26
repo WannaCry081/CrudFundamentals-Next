38f126981d8481583d0ac931bf40e3690c801b06
Lirae Que Data <liraedata59@gmail.com>
2024-08-26 23:46:50 +0800

    Update employee schema properties

--- schemas/employee/EmployeeSchema.ts
+++ schemas/employee/EmployeeSchema.ts
@@ -1,8 +1,12 @@
 import * as z from "zod";
 
 export const EmployeeSchema = z.object({
+  userName: z.string(),
   firstName: z.string(),
   lastName: z.string(),
+  email: z.string(),
+  position: z.string(),
+  phoneNumber: z.string(),
 });
 
-export type Employee = z.infer<typeof EmployeeSchema>;
+export type EmployeeSchemaType = z.infer<typeof EmployeeSchema>;
