13ec28a5619e56cff16bfdd7107dc1328d5c1001
Lirae Que Data <liraedata59@gmail.com>
2024-08-27 03:00:43 +0800

    Add employee form state component export

--- components/Employee/index.ts
+++ components/Employee/index.ts
@@ -1,3 +1,4 @@
 export { default as EmployeeDetails } from "@/components/employee/EmployeeDetails";
 export { default as EmployeeForm } from "@/components/employee/EmployeeForm";
 export { default as EmployeeList } from "@/components/employee/EmployeeList";
+export { default as EmployeeFormState } from "@/components/employee/EmployeeFormState";
