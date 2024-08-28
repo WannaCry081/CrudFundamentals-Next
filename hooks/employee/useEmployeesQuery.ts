0566be620d03ce918f70bceb1fabc1b10197c54e
Lirae Que Data <liraedata59@gmail.com>
2024-08-28 16:19:08 +0800

    Add pagination response

--- hooks/employee/useEmployeesQuery.ts
+++ hooks/employee/useEmployeesQuery.ts
@@ -1,11 +1,11 @@
-import { Employee } from "@/types";
+import type { Employee, Pagination } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { EmployeeService } from "@/services";
 
-const useEmployeesQuery = () => {
+const useEmployeesQuery = (pagination: Pagination) => {
   const { data, error, isLoading, isError } = useQuery<Employee[]>({
     queryKey: ["employees"],
-    queryFn: () => EmployeeService.list(),
+    queryFn: () => EmployeeService.list(pagination),
   });
 
   return { data, error, isLoading, isError };
