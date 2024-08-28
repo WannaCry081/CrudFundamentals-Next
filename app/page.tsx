844bd919b0e74bad0022c7a6c8adbed182303620
Lirae Que Data <liraedata59@gmail.com>
2024-08-28 13:28:03 +0800

    Add employees page redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
+  const router = useRouter();
 
-    </main>
-  );
+  useEffect(() => {
+    const redirectToEmployees = () => router.push("/employees");
+
+    redirectToEmployees();
+  }, [router]);
+
+  return <></>;
 }
